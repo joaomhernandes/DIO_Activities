0fbdf2c955cee80cb024cc1cd643709db5cb76e8
João Maurício Hernandes Carrenho <78742654+joaomhernandes@users.noreply.github.com>
2022-01-20 21:25:40 -0300

    Update project using Lombok

--- GFTStart3Java/padroesDeProjetoJava/padroesProjetoSpring/src/main/java/me/dio/padroesProjetoSpring/model/Cliente.java
+++ GFTStart3Java/padroesDeProjetoJava/padroesProjetoSpring/src/main/java/me/dio/padroesProjetoSpring/model/Cliente.java
@@ -9,7 +9,8 @@ import javax.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
-
+@Getter
+@Setter
 @Entity
 public class Cliente {
 	
@@ -21,29 +22,6 @@ public class Cliente {
 	@ManyToOne
 	private Endereco endereco;
 	
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getNome() {
-		return nome;
-	}
-
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
-
-	public Endereco getEndereco() {
-		return endereco;
-	}
-
-	public void setEndereco(Endereco endereco) {
-		this.endereco = endereco;
-	}
-
+	
 }
 	
